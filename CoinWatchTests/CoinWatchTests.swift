cdc7a90eccf987af3b3655484f8f8bf06c25fb3c
masteribro <mohammedibrahim075@gmail.com>
2025-05-22 17:10:28 +0100

    Initial Commit

---
+++ CoinWatchTests/CoinWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoinWatchTests.swift
+//  CoinWatchTests
+//
+//  Created by Ibrahim Mohammed on 22/05/2025.
+//
+
+import Testing
+
+struct CoinWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
